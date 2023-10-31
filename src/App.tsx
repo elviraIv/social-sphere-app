2b59dd12542a8c8808e4dcd8da87e430d12f850d
Elvira <elvira.n.ivanova@gmail.com>
2023-10-31 22:54:56 +0200

    Add base route structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,22 @@
+import { Route, Routes } from "react-router-dom";
+import SigninForm from "./_auth/forms/SigninForm";
+import SignupForm from "./_auth/forms/SignupForm";
+import { Home } from "./_root/pages";
+import "./globals.css";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <main className="flex h-screen">
+      <Routes>
+        {/* public routes */}
+        <Route path="/sign-in" element={<SigninForm />} />
+        <Route path="/sign-up" element={<SignupForm />} />
 
-export default App
+        {/* private routes */}
+        <Route index element={<Home />} />
+      </Routes>
+    </main>
+  );
+};
+
+export default App;
