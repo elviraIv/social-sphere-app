0d65fe4be49878ccd2435d1589e837ae7778d566
Elvira <elvira.n.ivanova@gmail.com>
2023-11-01 09:18:43 +0200

    Add sign-up layout

---
+++ src/lib/validation/index.ts
@@ -0,0 +1,9 @@
+import * as z from "zod";
+
+export const SignupValidation = z.object({
+    name: z.string().min(2, { message: 'Name is too short.' }),
+    username: z.string().min(2, { message: 'Username is too short.' }),
+    email:z.string().email(),
+    password: z.string().min(8,
+        { message: 'Password must be at least 8 characters.' })
+});
