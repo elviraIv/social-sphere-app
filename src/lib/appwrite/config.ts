c789a0440fdc988a34485ffc168fa8f35fbcecc9
Elvira <elvira.n.ivanova@gmail.com>
2023-11-01 11:14:04 +0200

    Set up appwrite

---
+++ src/lib/appwrite/config.ts
@@ -0,0 +1,24 @@
+import {Client, Account, Storage, Avatars, Databases} from 'appwrite'
+
+export const appwriteConfig = {
+    url: import.meta.env.VITE_APPWRITE_URL,
+    projectId: import.meta.env.VITE_APPWRITE_PROJECT_ID,
+    databaseId: import.meta.env.VITE_APPWRITE_DAATABASE_ID,
+    storageId:import.meta.env.VITE_APPWRITE_STORAGE_ID,
+    userCollectionId: import.meta.env.VITE_APPWRITE_USER_COLLECTION_ID,
+    postCollectionId: import.meta.env.VITE_APPWRITE_POST_COLLECTION_ID,
+    saveCollectionId: import.meta.env.VITE_APPWRITE_SAVE_COLLECTION_ID,
+
+}
+
+export const client = new Client();
+
+
+
+client.setProject(appwriteConfig.projectId);
+client.setEndpoint(appwriteConfig.url)
+
+export const account = new Account(client);
+export const databases = new Databases(client);
+export const storage = new Storage(client);
+export const avatars = new Avatars(client);
