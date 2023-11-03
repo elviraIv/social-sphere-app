fb3f5d528256296c40378237c93ff1e4cf77d9c0
Elvira <elvira.n.ivanova@gmail.com>
2023-11-03 09:21:24 +0200

    Update functionality for save/unsave posts

--- src/components/shared/PostStats.tsx
+++ src/components/shared/PostStats.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import {
   useDeleteSavedPost,
   useGetCurrentUser,
@@ -7,6 +7,7 @@ import {
 } from "@/lib/react-query/queries";
 import { checkIsLiked } from "@/lib/utils";
 import { Models } from "appwrite";
+import Loader from "./Loader";
 
 type PostStatsProps = {
   post: Models.Document;
@@ -20,10 +21,18 @@ const PostStats = ({ post, userId }: PostStatsProps) => {
   const [isSaved, setIsSaved] = useState(false);
 
   const { mutate: likePost } = useLikePost();
-  const { mutate: savePost } = useSavePost();
+  const { mutate: savePost, isPending: isSavingPost } = useSavePost();
   const { mutate: deleteSavedPost } = useDeleteSavedPost();
 
   const { data: curretUser } = useGetCurrentUser();
+
+  const savedPostRecord = curretUser?.save.find(
+    (record: Models.Document) => record.post.$id === post.$id
+  );
+
+  useEffect(() => {
+    setIsSaved(!!savedPostRecord);
+  }, [savedPostRecord]);
 
   const handleLikePost = (e: React.MouseEvent) => {
     e.stopPropagation();
@@ -44,14 +53,9 @@ const PostStats = ({ post, userId }: PostStatsProps) => {
   const handleSavePost = (e: React.MouseEvent) => {
     e.stopPropagation();
 
-    const savedPostRecord = curretUser?.save.find(
-      (record: Models.Document) => record.$id === post.$id
-    );
-
     if (savedPostRecord) {
       setIsSaved(false);
       deleteSavedPost(savedPostRecord.$id);
-      
     } else {
       savePost({ postId: post.$id, userId });
       setIsSaved(true);
@@ -73,20 +77,25 @@ const PostStats = ({ post, userId }: PostStatsProps) => {
           onClick={handleLikePost}
           className="cursor-pointer"
         />
+
         <p className="small-medium lg:base-medium">{likes.length}</p>
       </div>
       <div className="flex gap-2">
-        <img
-          src={`${
-            isSaved ? "/assets/icons/saved.svg" : "/assets/icons/save.svg"
-          }
+        {isSavingPost  ? (
+          <Loader />
+        ) : (
+          <img
+            src={`${
+              isSaved ? "/assets/icons/saved.svg" : "/assets/icons/save.svg"
+            }
         `}
-          alt="save"
-          width={20}
-          height={20}
-          onClick={handleSavePost}
-          className="cursor-pointer"
-        />
+            alt="save"
+            width={20}
+            height={20}
+            onClick={handleSavePost}
+            className="cursor-pointer"
+          />
+        )}
       </div>
     </div>
   );
