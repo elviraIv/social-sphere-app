29645c94372458a230886168a044b04446c872aa
Elvira <elvira.n.ivanova@gmail.com>
2023-11-07 11:51:31 +0200

    Update

--- src/lib/react-query/queries.ts
+++ src/lib/react-query/queries.ts
@@ -151,7 +151,7 @@ export const useGetPosts = () => {
         }
   
         // Use the $id of the last document as the cursor.
-        const lastId = lastPage.documents[lastPage.documents.length - 1].$id;
+        const lastId = lastPage?.documents[lastPage.documents.length - 1].$id;
         return lastId;
       },
     });
